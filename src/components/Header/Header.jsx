f051ae4797ed9891f7914eef875244d4c4887430
unknown <gausulislam5@gmail.com>
2023-10-05 17:53:00 +0600

    Update d header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Header.css';
+import logo from '/profile.png'
 
 const Header = () => {
     return (
         <nav>
             <h1>The Learning knowledge</h1>
+            <div className="navbar">
+                <a href="/home">Home</a>
+                <a href="/blog">Blog</a>
+                <a href="/aboutUs">About Us</a>
+                <a href="/inventory">Inventory</a>
+                <img src={logo} alt="" />
+            </div>
         </nav>
     );
 };
